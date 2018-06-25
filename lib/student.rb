0eefd04ac50f2e95c12e1572df144f65d0f993d4
Eric DeSimone <eric.desimone@cortland.edu>
2018-06-25 11:50:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,5 +24,13 @@ class Student
     DB[:conn].execute("DROP TABLE students")
   end
 
+  def save
+    sql = <<-SQL
+    INSERT INTO students (name, grade)
+    VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
 
 end
