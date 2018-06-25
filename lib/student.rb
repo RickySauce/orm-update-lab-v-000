7f609e0182d9baff69e7c0f41f5f47f53722d26b
Eric DeSimone <eric.desimone@cortland.edu>
2018-06-25 12:03:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,4 +63,10 @@ class Student
     end.first
   end
 
+  def update
+    sql = "UPDATE songs SET name = ?, album = ? WHERE id = ?"
+   DB[:conn].execute(sql, self.name, self.album, self.id)
+  end
+  
+
 end
